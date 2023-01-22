75ca4eddb9b643b1f125c92cc9e419a08a258b9d
Muhammad Farid Ullah <faridkhan125janan@gmail.com>
2023-01-22 12:10:59 +0500

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Muhammad Farid Ullah on 22/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
